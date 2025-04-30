98336008912b9a872c9de641afdfbfd399d370a2
Enae-data-diary <dudqns0512@naver.com>
2025-05-01 00:06:35 +0900

    0430 공부

---
+++ Python/세대.py
@@ -0,0 +1,12 @@
+
+result = 0
+while True:
+    num = int(input('숫자 입력:',))
+    if num >= 0:
+        result += num
+    else:
+        result += num
+        break
+
+print('총합:',result)
+    
---
+++ Python/연습용.py
@@ -0,0 +1,5 @@
+multiple = 0
+for n in range(1,30):
+    if n % 2 == 0 or n % 5 == 0:
+        multiple += n
+print(multiple)
